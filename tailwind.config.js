6917bb8f578ce6eec5bcbd9debbf332935e1b6ad
Andrus Peegel <andrusp@tlu.ee>
2023-04-23 17:29:57 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      'green' : '#2EB875',
+      'darkgreen' : '#249e64',
+      'blue': '#1fb6ff',
+      'purple': '#6675ff',
+      'lightpurple': '#4f5cd1',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'yellow': '#ffc82c',
+      'gray-dark': '#747a82',
+      'soft-gray': '#f9fafb',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'black': '#000000',
+      'white': '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
